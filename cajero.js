3f05ef05581d04917aee86791d6618453f67817f
DMoreno06 <mordiana1506@gmail.com>
2019-06-25 15:47:15 -0500

    funcion para entregar dinero

--- cajero.js
+++ cajero.js
@@ -4,14 +4,39 @@ class Billete{
         this.cantidad = c;
     }
 }
+function entregarDinero(){
+    for (const bi of caja) 
+    {
+        if(dinero > 0){
+            div = Math.floor(dinero/bi.valor);
+            if(div > bi.cantidad){
+                efectivo = bi.cantidad;
+            }else{
+                efectivo = div;
+            }
 
+            entregado.push(new Billete(bi.valor, efectivo));
+            dinero = dinero-(bi.valor * efectivo);
+        }
+        
+    }
+
+    if(dinero > 0){
+        console.log("estoy tirado")();
+    }
+    else{
+        for(var e of entregado){
+            document.write(e.cantidad+ " billetes de $" + e.valor);
+        }
+    }
+}
 var caja = [];
 var entregado = [];
-caja.push( new Billete(50,3));
-caja.push( new Billete(20,2));
-caja.push( new Billete(10,2));
+caja.push( new Billete(50,30));
+caja.push( new Billete(20,20));
+caja.push( new Billete(10,20));
 
-var dinero = 210;
+var dinero;
 var div = 0;
 var efectivo = 0;
 

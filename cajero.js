53b1e53290b6aa0d000d3cfec77afdb57e63b327
DMoreno06 <mordiana1506@gmail.com>
2019-06-25 17:47:43 -0500

    cajero sin treas

--- cajero.js
+++ cajero.js
@@ -4,29 +4,39 @@ class Billete{
         this.cantidad = c;
     }
 }
-function entregarDinero(){
+function entregarDinero()
+{
+    var t = document.getElementById("dinero");
+    dinero = parseInt(t.value);
     for (const bi of caja) 
     {
-        if(dinero > 0){
-            div = Math.floor(dinero/bi.valor);
+        if(dinero > 0)
+        {
+            div = Math.floor(dinero / bi.valor);
             if(div > bi.cantidad){
                 efectivo = bi.cantidad;
-            }else{
+            }
+            else
+            {
                 efectivo = div;
             }
 
             entregado.push(new Billete(bi.valor, efectivo));
-            dinero = dinero-(bi.valor * efectivo);
+            dinero = dinero - (bi.valor * efectivo);
         }
         
     }
 
     if(dinero > 0){
-        console.log("estoy tirado")();
+        
+        resultado.innerHTML =" No tengo dinero ";
     }
     else{
         for(var e of entregado){
-            document.write(e.cantidad+ " billetes de $" + e.valor);
+            if(e.cantidad > 0){
+                resultado.innerHTML += e.cantidad+ " billetes de $" + e.valor + "<br>";
+            }
+            
         }
     }
 }
@@ -36,9 +46,10 @@ caja.push( new Billete(50,30));
 caja.push( new Billete(20,20));
 caja.push( new Billete(10,20));
 
-var dinero;
+var dinero = 0;
 var div = 0;
 var efectivo = 0;
 
+var resultado = document.getElementById("resultado");
 var boton = document.getElementById("extraer");
 boton.addEventListener("click", entregarDinero);

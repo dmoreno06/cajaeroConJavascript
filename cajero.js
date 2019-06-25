350fd47bdda62dd704c7969ebc04a6c0f6cd8297
DMoreno06 <mordiana1506@gmail.com>
2019-06-25 14:50:23 -0500

    declaracion de variables js

---
+++ cajero.js
@@ -0,0 +1,13 @@
+
+var caja = [];
+var entregado = [];
+caja.push( new Billete(50,3));
+caja.push( new Billete(20,2));
+caja.push( new Billete(10,2));
+
+var dinero = 210;
+var div = 0;
+var efectivo = 0;
+
+var boton = document.getElementById("extraer");
+boton.addEventListener("click", entregarDinero);
